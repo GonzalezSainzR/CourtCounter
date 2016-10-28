48f60748fe2fc7e8b996c30d7f41e8f9b9514820
Renan Gonzalez-Sainz <raygs2000@gmail.com>
2016-10-28 15:04:14 -0700

    Yes

--- app/src/main/java/com/example/student/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/student/courtcounter/MainActivity.java
@@ -1,7 +1,15 @@
 package com.example.student.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-}
+
+    public void buttonOnClick(View v){
+        Button button=(Button) v;
+        ((Button) v).setText("clicked");
+    }
+
+};
+
